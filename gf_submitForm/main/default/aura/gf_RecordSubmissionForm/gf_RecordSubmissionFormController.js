f0386818948ae866df335ee7bfb0cfdbc093292c
Martin Kona <169martink@gmail.com>
2018-02-14 17:04:06 +0100

    added toogle mechanism for lightning card

--- gf_submitForm/main/default/aura/gf_RecordSubmissionForm/gf_RecordSubmissionFormController.js
+++ gf_submitForm/main/default/aura/gf_RecordSubmissionForm/gf_RecordSubmissionFormController.js
@@ -2,5 +2,14 @@
 	doInit : function(cmp, event, helper) {
 		
         helper.getDataConfiguration(cmp);
-	}
+	},
+
+    cardToggle : function(component, event, helper) {
+
+        var cardSection = component.find('showHideSection');
+        $A.util.toggleClass(cardSection,'slds-is-collapsed');
+
+        var sectionIcon = component.find('lockButton');
+        $A.util.toggleClass(sectionIcon,'fa-rotate-90');
+    }
 })
