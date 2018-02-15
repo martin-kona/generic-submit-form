aa21a38c392938cfdcfd45c0cf3e1fa8045e8f0f
Martin Kona <169martink@gmail.com>
2018-02-15 10:14:49 +0100

    creating separate component for light ing card

--- gf_submitForm/main/default/aura/DynamicFormSection/DynamicFormSectionController.js
+++ gf_submitForm/main/default/aura/DynamicFormSection/DynamicFormSectionController.js
@@ -2,5 +2,14 @@
 	doInit : function(cmp, event, helper) {
 		
         helper.initForm(cmp);
-	}
+	},
+
+    cardToggle : function(component) {
+
+        var cardSection = component.find('showHideSection');
+        $A.util.toggleClass(cardSection,'slds-is-collapsed');
+
+        var sectionIcon = component.find('lockButton');
+        //$A.util.toggleClass(sectionIcon, 'fa-rotate-90');
+    }
 })
--- gf_submitForm/main/default/aura/gf_RecordSubmissionForm/gf_RecordSubmissionFormController.js
+++ gf_submitForm/main/default/aura/gf_RecordSubmissionForm/gf_RecordSubmissionFormController.js
@@ -2,14 +2,5 @@
 	doInit : function(cmp, event, helper) {
 		
         helper.getDataConfiguration(cmp);
-	},
-
-    cardToggle : function(component) {
-
-        var cardSection = component.find('showHideSection');
-        $A.util.toggleClass(cardSection,'slds-is-collapsed');
-
-        var sectionIcon = component.find('lockButton');
-        $A.util.toggleClass(sectionIcon,'fa-rotate-90');
-    }
+	}
 })
